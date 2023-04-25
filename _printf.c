d7f076f872aa299dff3d110fe831707a7739d226
Dagmawit Yikber <dagiiyakk@gmail.com>
2023-04-25 15:56:15 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -38,11 +38,7 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-<<<<<<< HEAD
 				flags, width, precision, size);
-=======
-					flags, width, precision, size);
->>>>>>> 373a9603da7c8425ca53dbcd83822baccbd6c0b9
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
